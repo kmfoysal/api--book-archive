8824ec26dc19434d6e5f7f6db598ec1419938f95
kmfoysal <faisal.cri@gmail.com>
2021-09-02 18:36:50 +0600

    url update

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const searchBook = () =>{
       else {
 
         // Fetch Data
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
         .then(response => response.json())
         .then(data => searchResults(data, data.docs));
